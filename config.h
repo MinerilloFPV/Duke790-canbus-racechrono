890448e39d6fde74bcc14f810ee67f0f57a72af1
MinerilloFPV <victorvb93@hotmail.com>
2026-04-19 18:43:39 +0200

    Update config.h

--- config.h
+++ config.h
@@ -1,47 +1,51 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// Bluetooth device name
-// If not defined, will default to "RaceChrono aa:bb:cc"
-// aa:bb:cc being the last three octets of the MAC address
-// #define DEVICE_NAME "RaceChrono DIY"
+// Nombre del dispositivo Bluetooth (coincide con tu .ino)
+#define DEVICE_NAME "KTM 790 DIY Track"
 
-// Serial logging level, select one from the following list :
-// LOG_LEVEL_EMERGENCY
-// LOG_LEVEL_ALERT
-// LOG_LEVEL_CRITICAL
-// LOG_LEVEL_ERROR
-// LOG_LEVEL_WARNING
-// LOG_LEVEL_NOTICE
-// LOG_LEVEL_INFO
-// LOG_LEVEL_DEBUG
+// Nivel de log por serial
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-// GPIO pins connected to the CAN transceiver
-#define RX_PIN 11
-#define TX_PIN 12
+// Pines GPIO para el transceptor CAN (Ajustados a tu ESP32)
+#define RX_PIN 4
+#define TX_PIN 5
 
-// Serial baud rate
+// Velocidad del puerto serie
 #define SERIAL_BAUD_RATE 115200
 
-// TWAI RX buffer size
+// Tamaño del buffer de recepción CAN
 #define TWAI_RX_QUEUE_LENGTH 128
 
-// Bluetooth 5.0 support, 1 to enable, 0 to disable
-// #define CONFIG_BT_NIMBLE_EXT_ADV 1
-
-// Limit the rate of messages sent to RaceChrono to x CAN messages each second
-// Can be further refined per PID through the list below
+// Frecuencia por defecto (en Hz) para canales no especificados
 #define DEFAULT_UPDATE_RATE_HZ 10
 
-// An optional list of PIDs and their associated rate limit.
-// Limits the update rate of the specified PID to the returned value.
+// Configuración de frecuencias específicas por ID de CAN
+// Esto optimiza el ancho de banda del Bluetooth
 uint8_t getUpdateRateHz(uint32_t can_id)
 {
     switch (can_id)
     {
+    case 0x120: // RPM y TPS
+        return 20;
 
-    // An example of how to configure this list is detailed in the README.
+    case 0x129: // Marcha engranada
+        return 5;
+
+    case 0x12B: // Velocidades de rueda e Inclinación (IMU)
+        return 25;
+
+    case 0x130: // Intervención de ABS
+        return 20;
+
+    case 0x290: // Presión de frenos (Necesita mucha resolución)
+        return 50;
+
+    case 0x450: // Nivel de control de tracción (MTC)
+        return 2;
+
+    case 0x540: // Temperatura del motor
+        return 1;
 
     default:
         return DEFAULT_UPDATE_RATE_HZ;
